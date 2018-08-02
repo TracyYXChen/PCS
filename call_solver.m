38b745a2acdf788d196a9cd5119755e42d435eb8
Tracy_YXChen <ychen151@terpmail.umd.edu>
2018-08-02 13:21:15 -0400

    main function

--- call_solver.m
+++ call_solver.m
@@ -1,8 +1,15 @@
+%define parameter
+pdb_coor = pdb_coor;
+pcs_exp = pcs_exp;
+pcs_file = 'data/pcs_exp_pred.txt';
 %call function
 guess = [0,0,0];
 options = optimset('TolFun',1e-9,'TolX',1e-9,'MaxFunEvals',1000000,'MaxIter',100000);
-%[position, Chi2]=fminsearch('pcs_solver',guess,options,fit_ratio,fit_coord,factor,R2dia,Htime);
-[position, Chi2]=fminsearch('pcs_solver',guess,options);
+[position, Chi2]=fminsearch(@(guess) pcs_solver(guess,pdb_coor,pcs_exp,pcs_file),guess,options);
+%compare with numbat
+exp_numbat = dlmread('data/pcs_exp_numbat.txt');
+numbat_chi2 = sum((exp_numbat(:,1) - exp_numbat(:,2)).^2);
+fprintf('Our chi square is %f, and Chi square of numbat is %f', Chi2, numbat_chi2)
 %arguments:
 %guess: 
 %fit_ratio: experimental data to fit
